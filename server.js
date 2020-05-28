13db28ad2f2598527eb1df1fb3b1c16a5e94589a
Mildred Gil <mi.mitel@hotmail.com>
2020-05-28 07:37:09 -0500

    server

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+// Serve the static files from the build folder
+app.use(express.static( __dirname + "/build"));
+//app.use('/material-dashboard-react', express.static(__dirname + "/build"));
+// Redirect all traffic to the index
+app.get("*", function(req, res){
+  res.sendFile(__dirname + "/build/index.html");
 });
 app.listen(port, () => {
    console.log('Server is up!');
