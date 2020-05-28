bc04629650f2fb34349391178cc74a2cb900aef6
Mildred Gil <mi.mitel@hotmail.com>
2020-05-28 07:23:08 -0500

    server

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-app.listen(port, () => {
-   console.log(`Server is up on port ${port}!`);
-});
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
